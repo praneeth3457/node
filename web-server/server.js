9708c7d6bf342fc82c9fa993be165b20a2e32e4f
Praneeth Veerlapally <praneeth7516@gmail.com>
2015-12-31 18:28:53 -0800

    .

--- web-server/server.js
+++ web-server/server.js
@@ -1,19 +1,11 @@
 var express = require('express');
 var app = express();
+var middleware = require('./middleware');
 var port = 3000;
 
-var middleware = {
-	requireAuthentication : function (req, res, next) {
-		console.log('Private route hit!');
-		next();
-	},
-	logger : function (req, res, next) {
-		var date = new Date().toString();
-		console.log('Request: ' + date + ' ' + req.method + ' ' + req.originalUrl);
-	}
-};
 
 app.use(middleware.logger);
+
 app.get('/about', middleware.requireAuthentication,  function (req, res) {
 	res.send('About us!');
 });
